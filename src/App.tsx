c03304f26f84def4f3f4a92aadd160ca4b973622
Ryan Wright <SivanoFFDS@gmail.com>
2021-08-11 22:27:55 +0700

    Place main structure to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
-function App() {
-  console.log(process.env);
-  return (
-    <div className="App"></div>
-  );
-}
+import { Layout } from "antd";
+import { FC } from "react";
+
+/** Main layout and not more. Just places for components */
+const App: FC = () => (
+  <Layout>
+    <Layout.Header>
+
+    </Layout.Header>
+    <Layout.Content>
+
+    </Layout.Content>
+  </Layout>
+);
 
 export default App;
