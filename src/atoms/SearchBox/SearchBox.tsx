667bd49b3243b1bd92fc6f37a019fffd934d6262
Ryan Wright <SivanoFFDS@gmail.com>
2021-08-11 23:07:53 +0700

    Add classnames

--- src/atoms/SearchBox/SearchBox.tsx
+++ src/atoms/SearchBox/SearchBox.tsx
@@ -1,5 +1,5 @@
-import { AutoComplete } from "antd";
-import { ComponentType, ReactElement } from "react";
+import classnames from "classnames";
+import { ComponentType } from "react";
 
 type PropsWitnClassName = {
   className?: string;
@@ -7,12 +7,13 @@ type PropsWitnClassName = {
 
 interface ISearchBoxProps<WrapperProps> {
   as: ComponentType<WrapperProps>;
+  wrapperProps: WrapperProps;
 }
 
 /** Only big */
-function SearchBox<WrapperProps extends PropsWitnClassName>({ as: Wrapper, ...rest }: ISearchBoxProps<WrapperProps> & WrapperProps): JSX.Element {
+function SearchBox<WrapperProps extends PropsWitnClassName>({ as: Wrapper, wrapperProps }: ISearchBoxProps<WrapperProps> & WrapperProps): JSX.Element {
   return (
-    <Wrapper {...rest}>
+    <Wrapper {...wrapperProps} className={classnames(wrapperProps.className)}>
       
     </Wrapper>
   );
